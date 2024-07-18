5ae41d15a6e632231b0370058725a3fb2d8f8da1
玄兴梦影 <1989718921@qq.com>
2024-07-18 23:56:34 +0800

    [feat] using docker deploy

--- src/test/java/xyz/mxue/printing/PrintingApplicationTests.java
+++ src/test/java/xyz/mxue/printing/PrintingApplicationTests.java
@@ -18,16 +18,16 @@ class PrintingApplicationTests {
 
     @Resource
     private TbStatisticsDetailsService statisticsDetailsService;
-
-    @Test
-    void contextLoads() {
-        System.out.println(DigestUtils.md5DigestAsHex("123456".getBytes()));
-    }
-
-    @Test
-    void testGetDayOfPrintfIncome() {
-        System.out.println(orderService.page(4, 5, 1, new TbPrintOrder()));
-    }
+//
+//    @Test
+//    void contextLoads() {
+//        System.out.println(DigestUtils.md5DigestAsHex("123456".getBytes()));
+//    }
+//
+//    @Test
+//    void testGetDayOfPrintfIncome() {
+//        System.out.println(orderService.page(4, 5, 1, new TbPrintOrder()));
+//    }
 
 
 
